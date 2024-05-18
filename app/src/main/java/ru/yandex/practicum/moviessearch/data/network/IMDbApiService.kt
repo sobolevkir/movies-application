0d4108bf20808cffc07b6c0137a9cc2682c191b1
Kirill Sobolev <sobolevkir@bk.ru>
2024-05-18 12:48:50 +0300

    Добавлен экран поиска людей из киноиндустрии и экран с информацией о приложении. Также добавлена навигационная панель BottomNavigationBar для перехода между экранами приложения.

--- app/src/main/java/ru/yandex/practicum/moviessearch/data/network/IMDbApiService.kt
+++ app/src/main/java/ru/yandex/practicum/moviessearch/data/network/IMDbApiService.kt
@@ -6,12 +6,16 @@ import retrofit2.http.Path
 import ru.yandex.practicum.moviessearch.data.dto.MovieCastResponse
 import ru.yandex.practicum.moviessearch.data.dto.MovieDetailsResponse
 import ru.yandex.practicum.moviessearch.data.dto.MoviesSearchResponse
+import ru.yandex.practicum.moviessearch.data.dto.NamesSearchResponse
 
 interface IMDbApiService {
 
     companion object {
         private const val IMDB_KEY = "k_zcuw1ytf"
     }
+
+    @GET("/en/API/SearchName/$IMDB_KEY/{expression}")
+    fun searchNames(@Path("expression") expression: String): Call<NamesSearchResponse>
 
     @GET("/en/API/SearchMovie/$IMDB_KEY/{expression}")
     fun searchMovies(@Path("expression") expression: String): Call<MoviesSearchResponse>
--- app/src/main/java/ru/yandex/practicum/moviessearch/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/yandex/practicum/moviessearch/data/network/RetrofitNetworkClient.kt
@@ -7,6 +7,7 @@ import ru.yandex.practicum.moviessearch.data.NetworkClient
 import ru.yandex.practicum.moviessearch.data.dto.MovieCastRequest
 import ru.yandex.practicum.moviessearch.data.dto.MovieDetailsRequest
 import ru.yandex.practicum.moviessearch.data.dto.MoviesSearchRequest
+import ru.yandex.practicum.moviessearch.data.dto.NamesSearchRequest
 import ru.yandex.practicum.moviessearch.data.dto.Response
 
 class RetrofitNetworkClient(
@@ -18,13 +19,17 @@ class RetrofitNetworkClient(
         if (isConnected() == false) {
             return Response().apply { resultCode = -1 }
         }
-        // Добавили ещё одну проверку
-        if ((dto !is MoviesSearchRequest) && (dto !is MovieDetailsRequest) && (dto !is MovieCastRequest)) {
+
+        if ((dto !is MoviesSearchRequest)
+            && (dto !is MovieDetailsRequest)
+            && (dto !is NamesSearchRequest)
+            && (dto !is MovieCastRequest)
+        ) {
             return Response().apply { resultCode = 400 }
         }
 
-        // Добавили в выражение when ещё одну ветку
         val response = when (dto) {
+            is NamesSearchRequest -> imdbService.searchNames(dto.expression).execute()
             is MoviesSearchRequest -> imdbService.searchMovies(dto.expression).execute()
             is MovieDetailsRequest -> imdbService.getMovieDetails(dto.movieId).execute()
             else -> imdbService.getFullCast((dto as MovieCastRequest).movieId).execute()
@@ -41,7 +46,8 @@ class RetrofitNetworkClient(
         val connectivityManager = context.getSystemService(
             Context.CONNECTIVITY_SERVICE
         ) as ConnectivityManager
-        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         if (capabilities != null) {
             when {
                 capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
